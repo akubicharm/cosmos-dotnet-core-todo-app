c199fde6d4f617b392cd72e8783608bdcb434dfb
Kei Omizo <kay.akubichan.dev@gmail.com>
2020-01-08 13:07:44 +0900

    upate for dotnet3.1

--- src/Startup.cs
+++ src/Startup.cs
@@ -12,6 +12,9 @@
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Hosting;
+
+    using static Microsoft.AspNetCore.Hosting.IWebHostEnvironment;
 
     public class Startup
     {
@@ -38,12 +41,12 @@
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
  
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddSingleton<ICosmosDbService>(InitializeCosmosClientInstanceAsync(Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
